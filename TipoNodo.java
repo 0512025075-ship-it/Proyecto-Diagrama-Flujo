fd78e69b0d2b5c73aa436db8b8b0e01720431c59
0512025006-ctrl <0512025006@alumnos.unp.edu.pe>
2026-07-24 13:47:06 -0500

    Update TipoNodo.java

--- TipoNodo.java
+++ TipoNodo.java
@@ -1,9 +1,6 @@
 package diagramador;
 
-/**
- * Representa los tipos de nodo que puede tener un diagrama de flujo.
- * Cada tipo determina la forma con la que se dibuja el nodo.
- */
+
 public enum TipoNodo {
     INICIO,      // óvalo
     FIN,         // óvalo
